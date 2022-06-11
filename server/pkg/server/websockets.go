e2659d22c4bb4e1005bd9a4302ff1f1441bebf8a
Tyler Vanderley <TBVanderley4545@users.noreply.github.com>
2022-06-11 10:40:38 -0400

    * Begin adding a way for the client to communicate with the server over socket messages.

--- server/pkg/server/websockets.go
+++ server/pkg/server/websockets.go
@@ -2,10 +2,10 @@ package server
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/TBVanderley4545/the-one-with-the-galton-board/pkg/galton"
 	"github.com/gorilla/websocket"
 )
 
@@ -15,7 +15,7 @@ var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool {
 	return origin == "http://localhost:3000"
 }}
 
-type SocketMessage struct {
+type ClientMessage struct {
 	MessageText string `json:"msg"`
 	TimeStamp   int    `json:"timestamp"`
 }
@@ -31,14 +31,12 @@ func SocketHandler(w http.ResponseWriter, r *http.Request) {
 
 	for {
 		messageType, message, err := conn.ReadMessage()
-		var decodedMessage SocketMessage
+		var decodedMessage ClientMessage
 
 		if err != nil {
 			log.Println("Error reading message: ", err)
 			break
 		}
-
-		log.Printf("Message: %s", message)
 
 		if err := json.Unmarshal(message, &decodedMessage); err != nil {
 			upgrader.Error(w, r, http.StatusBadRequest, err)
@@ -46,7 +44,30 @@ func SocketHandler(w http.ResponseWriter, r *http.Request) {
 			log.Print("Error parsing json: ", err)
 		}
 
-		fmt.Println(decodedMessage.TimeStamp)
+		if decodedMessage.MessageText == "new observer" {
+			log.Println("We have a new observer!!")
+
+			if len(galton.CurrentBoard.Columns) == 0 {
+				log.Println("Creating new board")
+				galton.CurrentBoard = galton.CreateBoard(10)
+			} else {
+				log.Println("Board already exists.")
+				log.Println(galton.CurrentBoard)
+			}
+
+			if boardState, err := json.Marshal(galton.CurrentBoard); err != nil {
+				log.Print("Error parsing json: ", err)
+
+				err = conn.WriteMessage(messageType, []byte(boardState))
+
+				if err != nil {
+					log.Println("Error writing message: ", err)
+					break
+				}
+			}
+		}
+
+		log.Println(decodedMessage.TimeStamp)
 
 		err = conn.WriteMessage(messageType, []byte("Message has been received"))
 
